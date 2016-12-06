f689b9959f86e8d7c5df676e51d0884a29cb8ab1
jaccarmac <jaccarmac@gmail.com>
2016-12-06 00:04:05 +0000

    Add the rest of the walking logic.
    FossilOrigin-Name: a943716f88b6e4d78d93f9515d1dc9a2facfb1d6

--- scratch.el
+++ scratch.el
@@ -5,7 +5,7 @@
           ((equal "L" direction) (cons :left (string-to-number distance))))))
 
 (equal '(:right . 2) (segmentspec->segment "R2"))
-(equal '(:left . 10) (segmentspec->segment "L10")) 
+(equal '(:left . 10) (segmentspec->segment "L10"))
 
 (defun pathspec->path (pathspec)
   (mapcar #'segmentspec->segment (split-string pathspec ", ")))
@@ -15,8 +15,44 @@
 (equal '((:right . 5) (:left . 5) (:right . 5) (:right . 3))
        (pathspec->path "R5, L5, R5, R3"))
 
+(defun new-direction (old-direction turn-direction)
+  (cond ((eq :left turn-direction)
+         (cond ((eq :north old-direction) :west)
+               ((eq :east old-direction) :north)
+               ((eq :south old-direction) :east)
+               ((eq :west old-direction) :south)))
+        ((eq :right turn-direction)
+         (cond ((eq :north old-direction) :east)
+               ((eq :east old-direction) :south)
+               ((eq :south old-direction) :west)
+               ((eq :west old-direction) :north)))))
+
+(eq :east (new-direction :south :left))
+(eq :east (new-direction :north :right))
+
+(defun walk-segment (position segment)
+  (let ((new-direction (new-direction (caddr position) (car segment)))
+        (distance (cdr segment))
+        (x (car position))
+        (y (cadr position)))
+    (cond ((eq :north new-direction) (list x
+                                           (+ y distance)
+                                           new-direction))
+          ((eq :east new-direction) (list (+ x distance)
+                                          y
+                                          new-direction))
+          ((eq :south new-direction) (list x
+                                           (- y distance)
+                                           new-direction))
+          ((eq :west new-direction) (list (- x distance)
+                                          y
+                                          new-direction)))))
+
 (defun howfarawayis (pathspec)
-  5)
+  (let ((destination (reduce #'walk-segment
+                             (pathspec->path pathspec)
+                             :initial-value '(0 0 :north))))
+    (+ (abs (car destination)) (abs (cadr destination)))))
 
 (= 5 (howfarawayis "R2, L3"))
 (= 2 (howfarawayis "R2, R2, R2"))
