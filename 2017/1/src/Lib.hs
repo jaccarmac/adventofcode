e5af8ecf3f3ed0698d9321b1c87a64850774248f
jaccarmac <jaccarmac@gmail.com>
2017-12-01 08:12:25 +0000

    Add first solution for this year woo!
    FossilOrigin-Name: 276ba57ee559c27fe7f8bf8a436f4afc730f733b531902b737bc16809829705c

---
+++ 2017/1/src/Lib.hs
@@ -0,0 +1,20 @@
+module Lib (inverseCaptcha, inverseHalfwayCaptcha) where
+
+inverseCaptcha :: Integer -> Integer
+inverseCaptcha number = sum (map fst (filter (\n -> fst n == snd n) (zip numerals (rotate 1 numerals))))
+  where numerals = digits number
+
+inverseHalfwayCaptcha :: Integer -> Integer
+inverseHalfwayCaptcha number = sum (map fst (filter (\n -> fst n == snd n) (zip numerals (rotate halfway numerals))))
+  where numerals = digits number
+        halfway = length numerals `div` 2
+
+-- Thanks to https://stackoverflow.com/a/3963286 !
+digits :: Integer -> [Integer]
+digits 0 = []
+digits number = digits (number `div` 10) ++ [number `mod` 10]
+
+-- Thanks to https://stackoverflow.com/a/16379034 !
+rotate :: Int -> [a] -> [a]
+rotate _ [] = []
+rotate n xs = zipWith const (drop n (cycle xs)) xs
