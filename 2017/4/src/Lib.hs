bdfa95ab9810f33132358336d73e2ee2cd62327f
jaccarmac <jaccarmac@gmail.com>
2017-12-04 07:36:40 +0000

    Solve second part of day 4.
    FossilOrigin-Name: 3da7ac0ca0952eddc976645f6188e35d110c90990a318c7caf06648cd152d159

--- 2017/4/src/Lib.hs
+++ 2017/4/src/Lib.hs
@@ -5,8 +5,8 @@ import           Data.List
 part1 :: [String] -> Int
 part1 puzzle = occurrences goodPassphrase puzzle
 
-part2 :: [String] -> Integer
-part2 _ = 2
+part2 :: [String] -> Int
+part2 puzzle = occurrences noAnagramPassphrase puzzle
 
 occurrences :: (a -> Bool) -> [a] -> Int
 occurrences predicate = length . filter predicate
@@ -14,3 +14,7 @@ occurrences predicate = length . filter predicate
 goodPassphrase :: String -> Bool
 goodPassphrase passphrase = length phraseWords == length (nub phraseWords)
   where phraseWords = words passphrase
+
+noAnagramPassphrase :: String -> Bool
+noAnagramPassphrase passphrase = length phraseSortedWords == length (nub phraseSortedWords)
+  where phraseSortedWords = map sort (words passphrase)
