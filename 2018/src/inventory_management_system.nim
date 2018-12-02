a1a9a6159e94197b4468a5652c8e879ecc311292
jaccarmac <jaccarmac@gmail.com>
2018-12-02 06:13:31 +0000

    Solve first half of second problem.
    FossilOrigin-Name: 3ca34275015d005d0470fb22065dca298376178f6cf3d83b40e67a755615e7c3

---
+++ 2018/src/inventory_management_system.nim
@@ -0,0 +1,25 @@
+import os
+import sequtils
+import strutils
+import tables
+
+let input = if paramCount() > 0: (readFile paramStr 1)[0..^2]
+    else: readAll stdin
+
+let ids = splitLines input
+
+var withDoubles = 0
+var withTriples = 0
+for id in ids:
+    var letters = initTable[char, int]()
+    for letter in id:
+        letters[letter] = if contains(letters, letter): letters[letter] + 1
+            else: 1
+    var doubles = false
+    var triples = false
+    for letter in keys letters:
+        if letters[letter] == 2: doubles = true
+        if letters[letter] == 3: triples = true
+    withDoubles = if doubles: withDoubles + 1 else: withDoubles
+    withTriples = if triples: withTriples + 1 else: withTriples
+echo withDoubles * withTriples
