30e8c64f51a18606d27d211a866e63b4fc107a84
jaccarmac <jaccarmac@gmail.com>
2018-12-02 05:51:03 +0000

    Readd code.
    FossilOrigin-Name: 292384f9eb888a5262b5104a3cc70fe712af740decfcc56b61cd3fa14b29ec6b

---
+++ 2018/src/chronal_calibration.nim
@@ -0,0 +1,34 @@
+import intsets
+import os
+import sequtils
+import strutils
+
+let input = if paramCount() > 0: readFile paramStr 1
+    else: readAll stdin
+
+func parseChange(line: string): (char, int) =
+    (line[0], parseInt line[1..^1])
+
+let changes = map(splitLines input[0..^2], parseChange)
+
+func changeFrequency(start: int, change: (char, int)): int =
+    case change[0]
+        of '+':
+            start + change[1]
+        of '-':
+            start - change[1]
+        else:
+            raise newException(AssertionError, "invalid leading character")
+
+echo foldl(changes, changeFrequency(a, b), 0)
+
+var frequency = 0
+var visited = initIntSet()
+block secondStar:
+    while true:
+        for change in changes:
+            incl(visited, frequency)
+            frequency = changeFrequency(frequency, change)
+            if contains(visited, frequency):
+                break secondStar
+echo frequency
