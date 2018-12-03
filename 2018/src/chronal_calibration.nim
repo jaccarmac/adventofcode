5991d7606aeb37694a711a5825a7eff1e949074a
jaccarmac <jaccarmac@gmail.com>
2018-12-03 04:46:35 +0000

    Slow but more functional solution for first day.
    FossilOrigin-Name: b715c7ef572c5067842c9ddb0dfd7728345079c6ec3d1939ebe88dd378de97ab

--- 2018/src/chronal_calibration.nim
+++ 2018/src/chronal_calibration.nim
@@ -1,34 +1,44 @@
-import intsets
 import os
 import sequtils
+import sets
 import strutils
 
-let input = if paramCount() > 0: (readFile paramStr 1)[0..^2]
-    else: readAll stdin
-
-func parseChange(line: string): (char, int) =
-    (line[0], parseInt line[1..^1])
-
-let changes = map(splitLines input, parseChange)
+let changes = map(splitLines(
+  if paramCount() > 0: (readFile paramStr 1)
+  else: readAll stdin
+)[0..^2]) do (line: string) -> (char, int): (line[0], parseInt line[1..^1])
 
 func changeFrequency(start: int, change: (char, int)): int =
-    case change[0]
-        of '+':
-            start + change[1]
-        of '-':
-            start - change[1]
-        else:
-            raise newException(AssertionError, "invalid leading character")
+  case change[0]
+  of '+':
+    start + change[1]
+  of '-':
+    start - change[1]
+  else:
+    raise newException(AssertionError, "invalid leading character")
 
 echo foldl(changes, changeFrequency(a, b), 0)
 
-var frequency = 0
-var visited = initIntSet()
-block secondStar:
-    while true:
-        for change in changes:
-            incl(visited, frequency)
-            frequency = changeFrequency(frequency, change)
-            if contains(visited, frequency):
-                break secondStar
-echo frequency
+func firstRevisited(
+  changes: seq[(char, int)], changeIndex, frequency: int, visited: HashSet[int]
+): (int, int, HashSet[int]) =
+    let newChangeIndex = if changeIndex >= len(changes) - 1: 0 else: changeIndex + 1
+    let newFrequency = changeFrequency(frequency, changes[changeIndex])
+    var newVisited = initSet[int]()
+    incl newVisited, frequency
+    if contains(visited, newFrequency): (changeIndex, newFrequency, visited)
+    else: (newChangeIndex, newFrequency, visited + newVisited)
+
+func firstRevisited(changes: seq[(char, int)]): int =
+  var changeIndex = 0
+  var frequency = 0
+  var visited = initSet[int]()
+  while true:
+    let nextArgs = firstRevisited(changes, changeIndex, frequency, visited)
+    if nextArgs[0] == changeIndex: return nextArgs[1]
+    else:
+      changeIndex = nextArgs[0]
+      frequency = nextArgs[1]
+      visited = nextArgs[2]
+
+echo firstRevisited(changes)
