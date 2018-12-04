9a02f8fa73dfaa52c70b68106d74a3e299bf08ff
jaccarmac <jaccarmac@gmail.com>
2018-12-04 23:22:02 +0000

    Convert third day.
    FossilOrigin-Name: a746b84a8d0939ccf3d598797cb672f697824ba9b3c4c0222ea3570b3cacd869

--- 2018/src/no_matter_how_you_slice_it.nim
+++ 2018/src/no_matter_how_you_slice_it.nim
@@ -1,16 +1,20 @@
-# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%203:%20No%20Matter%20How%20You%20Slice%20It][Day 3: No Matter How You Slice It:2]]
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%203:%20No%20Matter%20How%20You%20Slice%20It][Day 3: No Matter How You Slice It:6]]
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-3-problem-line][day-3-problem-line]]
+type
+  Claim = tuple[id, x, y, width, height: int]
+  ProblemLine = Claim
+# day-3-problem-line ends here
+
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::read-problem-lines][read-problem-lines]]
 import os
 import sequtils
-import sets
 import strutils
-import tables
 
-type Claim = tuple[id, x, y, width, height: int]
-
-let claims = map(splitLines(
-  if paramCount() > 0: (readFile paramStr 1)
-  else: readAll stdin
-)[0..^2]) do (line: string) -> Claim:
+let problem = (
+  if paramCount() > 0: readFile paramStr 1 else: readAll stdin
+)[0..^2].splitLines.map do (line: string) -> ProblemLine:
+# read-problem-lines ends here
+  # [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-3-parse-line][day-3-parse-line]]
   let segments = splitWhitespace line
   result.id = parseInt segments[0][1..^1]
   let coords = split(segments[2], ",")
@@ -19,6 +23,11 @@ let claims = map(splitLines(
   let dims = split(segments[3], "x")
   result.width = parseInt dims[0]
   result.height = parseInt dims[1]
+  # day-3-parse-line ends here
+
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-3-solution-1][day-3-solution-1]]
+import sets
+import tables
 
 var fabric = initTable[(int, int), HashSet[int]]()
 
@@ -27,7 +36,7 @@ iterator coordsFor(claim: Claim): (int, int) =
     for y in countup(claim.y, claim.y + claim.height - 1):
       yield (x, y)
 
-for claim in claims:
+for claim in problem:
   for coord in coordsFor claim:
     let x = coord[0]
     let y = coord[1]
@@ -39,11 +48,14 @@ let contestedClaims =
   filter(toSeq values fabric) do (cs: HashSet[int]) -> bool: len(cs) > 1
 
 echo len contestedClaims
+# day-3-solution-1 ends here
 
-var goodClaims = toSet map(claims) do (c: Claim) -> int: c.id
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-3-solution-2][day-3-solution-2]]
+var goodClaims = toSet map(problem) do (c: Claim) -> int: c.id
 
 for contestedClaim in contestedClaims:
   excl goodClaims, contestedClaim
 
 echo goodClaims
-# Day 3: No Matter How You Slice It:2 ends here
+# day-3-solution-2 ends here
+# Day 3: No Matter How You Slice It:6 ends here
