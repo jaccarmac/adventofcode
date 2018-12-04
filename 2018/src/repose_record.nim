a86998fec611f9ccb370d11e6c30b3221d1877d1
jaccarmac <jaccarmac@gmail.com>
2018-12-04 06:28:25 +0000

    Incomplete and buggy first solution.
    FossilOrigin-Name: 0cbd154ccf53d150b86af5123f1f5d4d34904b33d1979fd896715f8a59b14afc

--- 2018/src/repose_record.nim
+++ 2018/src/repose_record.nim
@@ -46,12 +46,42 @@ let unsortedRecords = (
     raise newException(AssertionError, "invalid leading character")
 
 let records = unsortedRecords.sorted do (x, y: ReposeRecord) -> int:
-  result = x.year.cmp y.year
+  result = cmp(x.year, y.year)
   if result == 0:
-    result = x.year.cmp y.year
+    result = cmp(x.year, y.year)
   if result == 0:
-    result = x.year.cmp y.year
+    result = cmp(x.year, y.year)
   if result == 0:
-    result = x.year.cmp y.year
+    result = cmp(x.year, y.year)
   if result == 0:
-    result = x.year.cmp y.year
+    result = cmp(x.year, y.year)
+
+var calendar = initTable[(int, int, int), (int, seq[int])]()
+var guards = initTable[int, CountTable[int]]()
+var currentGuard: int
+var lastAsleep: int
+
+echo records[0].kind
+
+for record in records:
+  let today = (record.year, record.month, record.day)
+  case record.kind
+  of rrDuty:
+    echo "d"
+    currentGuard = record.id
+    echo currentGuard
+    if not guards.contains currentGuard:
+      guards[currentGuard] = initCountTable[int]()
+    if not calendar.contains today:
+      calendar[today] = (currentGuard, @[])
+  of rrSleep:
+    echo "s"
+    lastAsleep = record.minute
+  of rrWake:
+    echo "w"
+    for minute in lastAsleep.countup(record.minute - 1):
+      if not guards[currentGuard].contains minute:
+        guards[currentGuard][minute] = 1
+      else:
+        guards[currentGuard][minute].inc
+      calendar[today][1].add minute
