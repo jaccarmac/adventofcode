d58f5aa7f31d6a9c3c39338e040db638ed83c01b
jaccarmac <jaccarmac@gmail.com>
2018-12-04 23:28:22 +0000

    Fourth day.
    FossilOrigin-Name: 698b7862c76004aacdeb43cd2f407011b4a17ee0c9ccc3bd8f89150cf1e7c7d3

--- 2018/src/repose_record.nim
+++ 2018/src/repose_record.nim
@@ -1,11 +1,5 @@
-# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%204:%20Repose%20Record][Day 4: Repose Record:2]]
-import algorithm
-import math
-import os
-import sequtils
-import strutils
-import tables
-
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%204:%20Repose%20Record][Day 4: Repose Record:6]]
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-4-problem-line][day-4-problem-line]]
 type
   ReposeRecordKind = enum
     rrDuty
@@ -21,10 +15,19 @@ type
     case kind: ReposeRecordKind
     of rrDuty: id: int
     of rrWake, rrSleep: nil
+  ProblemLine = ReposeRecord
+# day-4-problem-line ends here
 
-let unsortedRecords = (
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::read-problem-lines][read-problem-lines]]
+import os
+import sequtils
+import strutils
+
+let problem = (
   if paramCount() > 0: readFile paramStr 1 else: readAll stdin
-)[0..^2].splitLines.map do (line: string) -> ReposeRecord:
+)[0..^2].splitLines.map do (line: string) -> ProblemLine:
+# read-problem-lines ends here
+  # [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-4-parse-line][day-4-parse-line]]
   result = ReposeRecordObj.new
   let dateSplit = line.split ']'
   let dateTimeSplit = dateSplit[0][1..^1].split ' '
@@ -46,8 +49,14 @@ let unsortedRecords = (
     result.kind = rrWake
   else:
     raise newException(AssertionError, "invalid leading character")
+  # day-4-parse-line ends here
 
-let records = unsortedRecords.sorted do (x, y: ReposeRecord) -> int:
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-4-solution-1][day-4-solution-1]]
+import algorithm
+import math
+import tables
+
+let records = problem.sorted do (x, y: ReposeRecord) -> int:
   result = x.year.cmp y.year
   if result == 0:
     result = x.month.cmp y.month
@@ -83,7 +92,9 @@ let sleepyGuards = toSeq(guards.pairs).sorted do (x, y: (int, CountTable[int])) 
 let sleepiestGuard = sleepyGuards[^1]
 
 echo sleepiestGuard[0] * sleepiestGuard[1].largest[0]
+# day-4-solution-1 ends here
 
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-4-solution-2][day-4-solution-2]]
 let consistentGuards = toSeq(guards.pairs).sorted do (x, y: (int, CountTable[int])) -> int:
   let xLargest = if x[1].len == 0: 0 else: x[1].largest[1]
   let yLargest = if y[1].len == 0: 0 else: y[1].largest[1]
@@ -92,4 +103,5 @@ let consistentGuards = toSeq(guards.pairs).sorted do (x, y: (int, CountTable[int
 let mostConsistentGuard = consistentGuards[^1]
 
 echo mostConsistentGuard[0] * mostConsistentGuard[1].largest[0]
-# Day 4: Repose Record:2 ends here
+# day-4-solution-2 ends here
+# Day 4: Repose Record:6 ends here
