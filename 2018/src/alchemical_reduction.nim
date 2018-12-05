d63de3efa2a2074b6ab06a6d1dc3ebcd47302c4d
jaccarmac <jaccarmac@gmail.com>
2018-12-05 18:23:10 +0000

    Hacky solution to second part.
    FossilOrigin-Name: a75c0f6df02586343aaa8a98d833a97aab9a354a9ae8d7a08f932d2e8dcac097

--- 2018/src/alchemical_reduction.nim
+++ 2018/src/alchemical_reduction.nim
@@ -1,32 +1,60 @@
-# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%205:%20Alchemical%20Reduction.][Day 5: Alchemical Reduction.:4]]
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%205:%20Alchemical%20Reduction.][Day 5: Alchemical Reduction.:5]]
 # [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::read-problem-stream][read-problem-stream]]
 import os
 import streams
 
-let problem = (
-  if paramCount() > 0: open paramStr 1 else: stdin
-).newFileStream
+let problem =
+  if paramCount() > 0:
+    (proc (): Stream =
+         let problemFile = open paramStr 1
+         newFileStream problemFile)
+  else:
+    let stdinString = readAll stdin
+    (proc (): Stream {.gcsafe.} = newStringStream stdinString)
 # read-problem-stream ends here
 
 # [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-5-solution-1][day-5-solution-1]]
 import deques
 import strutils
 
-var reagents = initDeque[char]()
-
-reagents.addLast readChar problem
-var nextReagent = readChar problem
-while nextReagent != '\n':
-  if reagents.len() > 0:
-    let previousReagent = peekLast reagents
-    if toLowerAscii(previousReagent) == toLowerAscii(nextReagent) and previousReagent != nextReagent:
-      popLast reagents
+func polymerLength(polymer: iterator(): char): int =
+  var reagents = initDeque[char]()
+  for nextReagent in polymer():
+    if reagents.len() > 0:
+      let previousReagent = peekLast reagents
+      if toLowerAscii(previousReagent) == toLowerAscii(nextReagent) and previousReagent != nextReagent:
+        popLast reagents
+      else:
+        reagents.addLast nextReagent
     else:
       reagents.addLast nextReagent
-  else:
-    reagents.addLast nextReagent
-  nextReagent = readChar problem
+  len reagents
 
-echo len reagents
+iterator solution1Polymer(): char {.closure.} =
+  let inStream = problem()
+  var nextReagent = readChar inStream
+  while nextReagent != '\n':
+    yield nextReagent
+    nextReagent = readChar inStream
+
+echo polymerLength(solution1Polymer)
 # day-5-solution-1 ends here
-# Day 5: Alchemical Reduction.:4 ends here
+
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-5-solution-2][day-5-solution-2]]
+func solution2Polymer(exclude: char): iterator(): char {.closure.} =
+  (iterator(): char {.closure.} =
+     let inStream = problem()
+     var nextReagent = readChar inStream
+     while nextReagent != '\n':
+       if toLowerAscii(nextReagent) != toLowerAscii(exclude):
+         yield nextReagent
+       nextReagent = readChar inStream)
+
+var minLength = high(int)
+
+for reagent in countup('a', 'z'):
+  minLength = minLength.min polymerLength solution2Polymer reagent
+
+echo minLength
+# day-5-solution-2 ends here
+# Day 5: Alchemical Reduction.:5 ends here
