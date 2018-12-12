bdc2ead9a0336db9685dbce67b9002b5b3984018
jaccarmac <jaccarmac@gmail.com>
2018-12-12 23:14:01 +0000

    Implement int-reading for the tree problem.
    FossilOrigin-Name: 3c2f76f1281481abeabb1c9b098dcbb80bfcf406d761f3789a4e7fe069554863

--- 2018/src/memory_maneuver.nim
+++ 2018/src/memory_maneuver.nim
@@ -1,3 +1,45 @@
-# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%208:%20Memory%20Maneuver][Day 8: Memory Maneuver:2]]
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::*Day%208:%20Memory%20Maneuver][Day 8: Memory Maneuver:5]]
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::read-problem-stream][read-problem-stream]]
+import os
+import streams
 
-# Day 8: Memory Maneuver:2 ends here
+var problem: proc(): Stream
+if paramCount() > 0:
+  problem = proc (): Stream =
+    let problemFile = open paramStr 1
+    newFileStream problemFile
+else:
+  let stdinString = readAll stdin
+  problem = proc (): Stream =
+    newStringStream stdinString
+# read-problem-stream ends here
+
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::problem-ints][problem-ints]]
+import strutils
+
+iterator problemInts(): int {.closure.} =
+  let inStream = problem()
+  var next = readChar inStream
+  var num = ""
+  while next != '\n':
+    if next == ' ':
+      yield parseInt num
+      num = ""
+    else:
+      num &= next
+    next = readChar inStream
+  yield parseInt num
+# problem-ints ends here
+
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-8-solution-1][day-8-solution-1]]
+var treeSource = problemInts
+while true:
+  let value = treeSource()
+  if finished treeSource: break
+  echo value
+# day-8-solution-1 ends here
+
+# [[file:~/src/src/jaccarmac.com/adventofcode/2018/advent-of-nim.org::day-8-solution-2][day-8-solution-2]]
+
+# day-8-solution-2 ends here
+# Day 8: Memory Maneuver:5 ends here
