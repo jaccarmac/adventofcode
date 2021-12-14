7aa56918630d1912fbf3bd101e159089ae24ed78
jaccarmac <jaccarmac@gmail.com>
2021-12-14 01:20:06 +0000

    Using a macro lets us do it.
    FossilOrigin-Name: 2d7b03792470d6dc7639fb8946cced3075bb4d1ed234b95c4bf8755fca8ac897

--- 2021/run-day.lisp
+++ 2021/run-day.lisp
@@ -3,8 +3,8 @@
 
 (april:april-load (pathname "advent-of-april.apl"))
 
-(defun print-solutions (day)
-  (let ((problem (vector 199 200 208 210 200 207 240 269 260 263)))
+(defmacro print-solutions (day)
+  `(let ((problem (vector 199 200 208 210 200 207 240 269 260 263)))
     (format t "~a~%~a~%"
-            (april:april-c "s1a" problem)
-            (april:april-c "s1b" problem))))
+            (april:april-c ,(format nil "s~aa" day) problem)
+            (april:april-c ,(format nil "s~ab" day) problem))))
--- 2021/run-day.sh
+++ 2021/run-day.sh
@@ -1,6 +1,6 @@
 DAY=$1
 
-sbcl --noinform --noprint \
+sbcl --noinform --noprint --disable-debugger \
      --load run-day.lisp \
      --eval "(print-solutions $DAY)" \
      --quit
