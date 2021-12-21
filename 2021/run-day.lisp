155828088d719d42b3fcb392669290eec02946ed
jaccarmac <jaccarmac@gmail.com>
2021-12-21 18:05:07 +0000

    Non-macro solution.
    FossilOrigin-Name: 9227da4918585313b9e62e33f465a28785889ff4e6f046e60d67911013d83ccc

--- 2021/run-day.lisp
+++ 2021/run-day.lisp
@@ -3,9 +3,9 @@
 
 (april:april-load (pathname "advent-of-april.apl"))
 
-(defmacro print-solutions (day)
-  `(with-open-file (input ,(format nil "data/~a" day))
-     (let ((puzzle (make-string (file-length input))))
-       (read-sequence puzzle input)
-       (april:april-c ,(format nil "{⎕←s~aa⍵◊⎕←s~ab⍵}" day day)
-                      puzzle))))
+(defun print-solutions (day)
+  (with-open-file (input (format nil "data/~a" day))
+    (let ((puzzle (make-string (file-length input))))
+      (read-sequence puzzle input)
+      (april:april (with (:store-val (p puzzle) (d day)))
+                   "⍎'⎕←s',(⍕d),'a p◊⎕←s',(⍕d),'b p'"))))
