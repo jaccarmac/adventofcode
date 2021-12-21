a2ea31370d2e6ee606e7fc3626d2bff6d7429a05
jaccarmac <jaccarmac@gmail.com>
2021-12-21 05:45:11 +0000

    Add utilities and a small refactor.
    FossilOrigin-Name: 27ca5c210f560d7ef54c59f24cb3b2828dd06bf41f8b993302b14fa818031f72

--- 2021/run-day.lisp
+++ 2021/run-day.lisp
@@ -3,8 +3,10 @@
 
 (april:april-load (pathname "advent-of-april.apl"))
 
-(defun print-solutions (day)
-  (with-open-file (input (format nil "data/~a" day))
-    (let ((puzzle (make-string (file-length input))))
-      (read-sequence puzzle input)
-      (april:april-c "solve" puzzle (write-to-string day)))))
+(defmacro print-solutions (day)
+  `(with-open-file (input ,(format nil "data/~a" day))
+     (let ((puzzle (make-string (file-length input))))
+       (read-sequence puzzle input)
+       (format t "~a~%~a~%"
+               (april:april-c ,(format nil "s~aa" day) puzzle)
+               (april:april-c ,(format nil "s~ab" day) puzzle)))))
