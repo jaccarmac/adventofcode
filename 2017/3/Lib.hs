6636b3f8250667ec4a4acdcc78e9ac2ccf6999de
jaccarmac <jaccarmac@gmail.com>
2022-05-23 00:52:37 +0000

    Remove unused, refactor cellNeighbors to be useful for part 2.
    FossilOrigin-Name: 27c30694d85a7788a1a79c5e8a37072227a640f343cadd89a7ee1e4444c317ba

--- 2017/3/Lib.hs
+++ 2017/3/Lib.hs
@@ -1,5 +1,8 @@
 module Lib (part1, part2) where
 
+import qualified Data.Map.Strict as Map
+import           Data.Maybe      (catMaybes)
+
 type Coordinate = (Integer, Integer)
 
 part1 :: Integer -> Integer
@@ -45,31 +48,5 @@ walkLeft (x, y) = (x - 1, y)
 walkRight :: Coordinate -> Coordinate
 walkRight (x, y) = (x + 1, y)
 
-minimumNeighbor :: Integer -> Integer
-minimumNeighbor cell = minimum (cellNeighbors cell)
-
-cellNeighbors :: Integer -> [Integer]
-cellNeighbors cell = [cellForCoord (walkUp coord), cellForCoord (walkDown coord), cellForCoord (walkLeft coord), cellForCoord (walkRight coord)]
-  where coord = coordForCell cell
-
-cellsInRing :: Integer -> [Integer]
-cellsInRing 0 = [1]
-cellsInRing ring = take (fromIntegral (ringsDimension ring ^ 2 -
-                         ringsDimension (ring - 1) ^ 2))
-                   (drop (fromIntegral (ringsDimension (ring - 1) ^ 2)) [1..])
-
-ringsDimension :: Integer -> Integer
-ringsDimension rings = rings * 2 + 1
-
-ringForCell :: Integer -> Integer
-ringForCell cell = head (filter (\r -> cell `elem` cellsInRing r) [1..])
-
-cellForCoord :: Coordinate -> Integer
-cellForCoord (0, 0) = 1
-cellForCoord (x, y) = 1 + cellForCoord previousCoord
-  where previousCoord
-          | x > 0 && (x == -y + 1 || x == -y) = (x - 1, y)
-          | x > 0 && x >= abs y = (x, y - 1)
-          | y > 0 && y >= abs x = (x + 1, y)
-          | x < 0 && x <= y = (x, y + 1)
-          | y < 0 && abs y >= x = (x - 1, y)
+cellNeighbors :: Coordinate -> Map.Map Coordinate Integer -> [Integer]
+cellNeighbors coord spiral = catMaybes [Map.lookup (walkUp coord) spiral, Map.lookup (walkDown coord) spiral, Map.lookup (walkLeft coord) spiral, Map.lookup (walkRight coord) spiral]
