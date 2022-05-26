c121857bc705b96dd9d61acea8421549c7fa305e
jaccarmac <jaccarmac@gmail.com>
2022-05-26 00:03:35 +0000

    Add a state type and an overwritten jump function.
    FossilOrigin-Name: 86f86b2d46ec20d15d103dd4b61ea2d43878e65bb89ce45f2a5d173bd540987b

--- 2017/5/Main.hs
+++ 2017/5/Main.hs
@@ -10,5 +10,16 @@ main = do
 part1 :: [Integer] -> [Integer]
 part1 = id
 
+data JumpState = Exited | Offsets [Integer] Integer [Integer] deriving (Show)
+
+jump :: JumpState -> JumpState
+jump Exited = Exited
+jump (Offsets p 0 s) = Offsets p 1 s
+jump (Offsets preceding offset succeeding)
+  | fromIntegral offset > length succeeding = Exited
+  | - fromIntegral offset > length preceding = Exited
+  | offset > 0 = Offsets (preceding ++ [offset + 1] ++ take (fromIntegral offset - 1) succeeding) (succeeding !! (fromIntegral offset - 1)) (drop (fromIntegral offset) succeeding)
+  | offset < 0 = Offsets (take (length preceding + fromIntegral offset) preceding) (preceding !! (length preceding + fromIntegral offset)) (drop (length preceding + fromIntegral offset + 1) preceding ++ [offset + 1] ++ succeeding)
+
 part2 :: [Integer] -> [Integer]
 part2 = part1
