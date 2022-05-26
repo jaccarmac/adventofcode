2632b65994f20cb903611c4aef19d9c96f9cfb46
jaccarmac <jaccarmac@gmail.com>
2022-05-26 23:09:07 +0000

    Add the second part
    It was even easier than I thought, reusing the first solution.
    
    FossilOrigin-Name: ba462f4ddffc23dbeedc528b8fd9fbf613c33b51d54c8c256105ecdb4acc43f4

--- 2017/6/Main.hs
+++ 2017/6/Main.hs
@@ -1,15 +1,22 @@
-import           Data.List (mapAccumL)
-import           Data.Set  (Set)
-import qualified Data.Set  as Set
+import           Data.List  (elemIndex, mapAccumL)
+import           Data.Maybe (fromMaybe)
+import           Data.Set   (Set)
+import qualified Data.Set   as Set
 
 main :: IO ()
 main = do
   puzzleContents <- readFile "puzzle.txt"
   let puzzle = map read $ words puzzleContents
   print $ part1 puzzle
+  print $ part2 puzzle
 
 part1 :: [Integer] -> Int
-part1 puzzle = length $ takeWhile not $ snd $ mapAccumL hasDuplicates Set.empty $ iterate distribute puzzle
+part1 puzzle = length $ takeWhile not $ snd $ mapAccumL hasDuplicates Set.empty $ states puzzle
+
+part2 :: [Integer] -> Int
+part2 puzzle = fromMaybe 0 $ fmap (part1' -) $ elemIndex (states' !! part1') states'
+  where states' = states puzzle
+        part1' = part1 puzzle
 
 distribute :: [Integer] -> [Integer]
 distribute memory = distributeFrom (maximum memory) [] memory
@@ -26,3 +33,6 @@ fillBanks remaining done (bank:rest) = fillBanks (remaining - 1) (done ++ [bank 
 
 hasDuplicates :: Set [Integer] -> [Integer] -> (Set [Integer], Bool)
 hasDuplicates set memory = (Set.insert memory set, Set.member memory set)
+
+states :: [Integer] -> [[Integer]]
+states puzzle = iterate distribute puzzle
