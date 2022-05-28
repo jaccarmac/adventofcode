dbe19eef7ea47031e13cac827cfad517665ecf09
jaccarmac <jaccarmac@gmail.com>
2022-05-28 00:31:54 +0000

    Greatly simplify parsing.
    The view patterns way is also fun, and I experimented with it before deciding
    on the no-pragma solution. Having to type words twice is a bit annoying; I
    might update with a case or something.
    
    FossilOrigin-Name: 01aa670c97263ccb4a0bc2e9425f5801762fa89d5dc92ce73d6c71bbd951cc45

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -1,25 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import           Data.Text (Text)
-import qualified Data.Text as T
+data Tower = Program String Int [Tower] deriving (Show)
 
 main :: IO ()
 main = do
   puzzleContents <- readFile "example.txt"
-  let puzzle = map (parseInitial . splitChildren . splitLine . T.pack) $ lines puzzleContents
+  let puzzle = map parseInitial $ lines puzzleContents
+        where parseInitial line | [n, w] <- words line = (Program n (read w) [], [])
+                                | (n:w:_:cs) <- words line = (Program n (read w) [], cs)
   print puzzle
-
-splitLine :: Text -> [Text]
-splitLine = T.splitOn "->"
-
-splitChildren :: [Text] -> (Text, [Text])
-splitChildren [name]            = (name, [])
-splitChildren (name:children:_) = (name, map T.strip $ T.splitOn "," children)
-
-data Tower = Program String Int [Tower] deriving (Show)
-
-parseInitial :: (Text, [Text]) -> (Tower, [String])
-parseInitial (t, cs) = let tokens = map T.strip $ T.splitOn "(" t
-                           name = T.unpack $ tokens !! 0
-                           weight = read $ T.unpack $ T.take (T.length (tokens !! 1) - 1) $ tokens !! 1
-                       in (Program name weight [], map T.unpack cs)
