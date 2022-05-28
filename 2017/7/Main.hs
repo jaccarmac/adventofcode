ad46980c255f4ca11db1fda6fcdb0ee6f30efe50
jaccarmac <jaccarmac@gmail.com>
2022-05-28 00:36:09 +0000

    Factor out the words line noise.
    FossilOrigin-Name: 72f676bfe0907356b8fda948629bd1c82c5f85cdceaef7dd079ce679a1069b2b

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -3,7 +3,7 @@ data Tower = Program String Int [Tower] deriving (Show)
 main :: IO ()
 main = do
   puzzleContents <- readFile "example.txt"
-  let puzzle = map parseInitial $ lines puzzleContents
-        where parseInitial line | [n, w] <- words line = (Program n (read w) [], [])
-                                | (n:w:_:cs) <- words line = (Program n (read w) [], cs)
+  let puzzle = map (parseInitial . words) $ lines puzzleContents
+        where parseInitial [n, w]     = (Program n (read w) [], [])
+              parseInitial (n:w:_:cs) = (Program n (read w) [], cs)
   print puzzle
