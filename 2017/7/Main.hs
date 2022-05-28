8cfc6fdae720f3ce9e5765e3b370b0212a0bf310
jaccarmac <jaccarmac@gmail.com>
2022-05-28 00:43:26 +0000

    Rename "children" to "above".
    FossilOrigin-Name: e0ae3ac67a6cb18be1054da1000474019c4d8ef480da404951ca47bed4b65c8f

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -5,6 +5,7 @@ main = do
   puzzleContents <- readFile "example.txt"
   let puzzle = map (parseInitial . words) $ lines puzzleContents
         where parseInitial (name:weight:rest) = (Program name (read weight) [],
-                                                 case rest of [] -> []
-                                                              (_:children) -> children)
+                                                 case rest of []        -> []
+                                                              (_:above) -> above)
   print puzzle
+
