5a9168297066f463daa48f70563105746285524a
jaccarmac <jaccarmac@gmail.com>
2022-05-30 05:53:10 +0000

    Use set operations to determine the root.
    FossilOrigin-Name: 50514a403358e0f9b11ac74488e8eb7ef4765c8f3f3dc6eb9203ff4545cc39ac

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -1,10 +1,11 @@
 import           Data.List (find)
+import qualified Data.Set  as S
 
 data Tower = Program String Int [Tower] deriving (Show)
 
 main :: IO ()
 main = do
-  puzzleContents <- readFile "example.txt"
+  puzzleContents <- readFile "puzzle.txt"
   let puzzle = (parseInitial . words) <$> lines puzzleContents
         where parseInitial (name:weight:rest) = (Program name (read weight) [],
                                                  case rest of []        -> []
@@ -12,7 +13,10 @@ main = do
   print $ part1 puzzle
 
 part1 :: [(Tower, [String])] -> Maybe Tower
-part1 = flip subTree "tknk"
+part1 puzzle = subTree puzzle =<< rootName
+  where rootName = S.lookupMin $ allNames `S.difference` childNames
+        allNames = S.fromList [n | (Program n _ _, _) <- puzzle]
+        childNames = S.unions [S.fromList ns | (_, ns) <- puzzle]
 
 subTree :: [(Tower, [String])] -> String -> Maybe Tower
 subTree fragments name = case children of Nothing -> Nothing
