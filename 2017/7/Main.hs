bb01c232b50c86b497065c6f4d25638559c6d356
jaccarmac <jaccarmac@gmail.com>
2022-05-30 05:13:06 +0000

    Start breaking things apart, put the sequence stuff out on its own.
    FossilOrigin-Name: fb20d8dadba7b5b841a2fd8659c1981ba905822e9ab9efa5c64045b93f0c6aa1

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -15,10 +15,10 @@ part1 :: [(Tower, [String])] -> Maybe Tower
 part1 = subTree "tknk"
 
 subTree :: String -> [(Tower, [String])] -> Maybe Tower
-subTree name fragments = do
-  found <- find (\(Program n _ _, _) -> n == name) fragments
-  let children = sequence $ map (flip subTree fragments) $ snd found
-  pure $ fst found
+subTree name fragments = fst <$> find (\(Program n _ _, _) -> n == name) fragments
+
+childSubTrees :: [String] -> [(Tower, [String])] -> Maybe [Tower]
+childSubTrees names fragments = sequence $ map (flip subTree fragments) names
 
 append :: Tower -> Tower -> Tower
 append t (Program rootName rootWeight rootChildren) = Program rootName rootWeight $ t:rootChildren
