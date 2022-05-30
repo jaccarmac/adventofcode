6de9654ecb1bfea3e600c4a5f1bf70baa27a7a32
jaccarmac <jaccarmac@gmail.com>
2022-05-30 06:01:21 +0000

    Apply a batch of lints.
    FossilOrigin-Name: efcbec3e902995e9702cb90159798763c26a823126c1cc7a9342623083e052a9

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -6,10 +6,10 @@ data Tower = Program String Int [Tower] deriving (Show)
 main :: IO ()
 main = do
   puzzleContents <- readFile "example.txt"
-  let puzzle = (parseInitial . words) <$> lines puzzleContents
+  let puzzle = parseInitial . words <$> lines puzzleContents
         where parseInitial (name:weight:rest) = (Program name (read weight) [],
                                                  case rest of []        -> []
-                                                              (_:above) -> (filter (`notElem` ",")) <$> above)
+                                                              (_:above) -> filter (`notElem` ",") <$> above)
   print $ part1 puzzle
 
 part1 :: [(Tower, [String])] -> Maybe String
@@ -24,12 +24,12 @@ treeFromPuzzle puzzle = subTree puzzle =<< rootName
 
 subTree :: [(Tower, [String])] -> String -> Maybe Tower
 subTree fragments name = case children of Nothing -> Nothing
-                                          Just ts -> (withChildren ts) . fst <$> found
+                                          Just ts -> withChildren ts . fst <$> found
   where found = find (\(Program n _ _, _) -> n == name) fragments
-        children = childSubTrees fragments =<< snd <$> found
+        children = childSubTrees fragments . snd =<< found
 
 childSubTrees :: [(Tower, [String])] -> [String] -> Maybe [Tower]
-childSubTrees fragments names = sequence $ (subTree fragments) <$> names
+childSubTrees fragments names = sequence $ subTree fragments <$> names
 
 withChildren :: [Tower] -> Tower -> Tower
 withChildren ts (Program rootName rootWeight _) = Program rootName rootWeight ts
