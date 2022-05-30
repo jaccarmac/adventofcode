58732dfab84424a5ed82e7dce31d138139526ca8
jaccarmac <jaccarmac@gmail.com>
2022-05-30 04:05:24 +0000

    Progress? I'm doing the find now, but not well.
    FossilOrigin-Name: 77c5d58824a1f889702a794afabe5157eeaaa0d01219445d506cd48782a44d85

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -1,3 +1,6 @@
+import           Data.List  (find)
+import           Data.Maybe (fromMaybe)
+
 data Tower = Program String Int [Tower] deriving (Show)
 
 main :: IO ()
@@ -9,8 +12,14 @@ main = do
                                                               (_:above) -> above)
   print $ part1 puzzle
 
-part1 :: [(Tower, [String])] -> Either [(Tower, [String])] String
-part1 [(Program name _ _, [])] = Right name
+part1 :: [(Tower, [String])] -> Tower
+part1 puzzle = subTree "tknk" puzzle
+
+subTree :: String -> [(Tower, [String])] -> Tower
+subTree name fragments = Program name weight children
+  where (Program _ weight children) = maybe (Program "" 0 []) fst $ find finder fragments
+        finder :: (Tower, [String]) -> Bool
+        finder ((Program n _ _), _) = n == name
 
 append :: Tower -> Tower -> Tower
 append t (Program rootName rootWeight rootChildren) = Program rootName rootWeight $ t:rootChildren
