8a3bb4e15e8fa2ff87504fa260bd2d577a8ec5b7
jaccarmac <jaccarmac@gmail.com>
2022-05-31 03:44:12 +0000

    Get it working.
    There's obvious code to factor out, but the real thing I want to do is figure
    out how to abstract over the repeated fst calls. I'm thinking it has something
    to do with lifting, since I believe functions are monads... But that's another
    project. Cobbled is OK for now.
    
    FossilOrigin-Name: 04ad5a6fec294756c67290b204ff1e6f0355ec0c27dddc7a8566bc9306a0df73

--- 2017/7/Main.hs
+++ 2017/7/Main.hs
@@ -1,7 +1,8 @@
-import           Data.List  (find, group, maximumBy)
-import           Data.Maybe (listToMaybe)
-import           Data.Ord   (comparing)
-import qualified Data.Set   as S
+import           Control.Arrow ((&&&))
+import           Data.List     (find, groupBy, maximumBy, minimumBy)
+import           Data.Maybe    (listToMaybe)
+import           Data.Ord      (comparing)
+import qualified Data.Set      as S
 
 data Tower = Program String Int [Tower] deriving (Show)
 
@@ -47,6 +48,9 @@ balanced (Program _ _ ts) = minimum weights == maximum weights
   where weights = totalWeight <$> ts
 
 part2' :: Tower -> Maybe Int
-part2' (Program _ _ ts) | all balanced ts = listToMaybe $ maximumBy (comparing length) $ group weights
+part2' (Program _ w ts) | all balanced ts = pure $ snd actualWeight + difference
                         | otherwise = part2' =<< find (not . balanced) ts
-                        where weights = totalWeight <$> ts
+                        where weights = (totalWeight &&& (\(Program _ w _) -> w)) <$> ts
+                              targetWeight = head $ maximumBy (comparing length) $ groupBy (\x y -> fst x == fst y) weights
+                              actualWeight = head $ minimumBy (comparing length) $ groupBy (\x y -> fst x == fst y) weights
+                              difference = fst targetWeight - fst actualWeight
