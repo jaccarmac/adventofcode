86e4bf71903d11abb3cacf0712b937fa7f98418f
jaccarmac <jaccarmac@gmail.com>
2023-02-22 03:15:12 +0000

    Use Guix to format the manifest.
    FossilOrigin-Name: a50c0aaf370ff10d4dae32c624ba14a2e9e2345e84441d9e08ff597f537ba14f

--- 2021/manifest.scm
+++ 2021/manifest.scm
@@ -8,26 +8,26 @@
     (package
       (inherit sbcl-april)
       (version (git-version "1.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/phantomics/april")
-               (commit commit)))
-         (file-name (git-file-name "cl-april" version))
-         (sha256
-          (base32 "00j5ghxb01rc110nim7yf02afc5mbnb36f0jxr7mbpl48s80yjh4"))
-         (modules '((guix build utils)))
-         (snippet '(begin
-                     ;; Remove bundled Apache-relicensed MaxPC.
-                     (delete-file-recursively "maxpc-apache")
-                     ;; Ensure references are to upstream MaxPC.
-                     (substitute* "vex/vex.asd"
-                       (("maxpc-apache") "maxpc"))))))
-      (inputs (cons (list "sbcl-serapeum" sbcl-serapeum) (cons (list "sbcl-cl-unicode" sbcl-cl-unicode)
-                                                               (package-inputs sbcl-april)))))))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/phantomics/april")
+                      (commit commit)))
+                (file-name (git-file-name "cl-april" version))
+                (sha256
+                 (base32
+                  "00j5ghxb01rc110nim7yf02afc5mbnb36f0jxr7mbpl48s80yjh4"))
+                (modules '((guix build utils)))
+                (snippet '(begin
+                            ;; Remove bundled Apache-relicensed MaxPC.
+                            (delete-file-recursively "maxpc-apache")
+                            ;; Ensure references are to upstream MaxPC.
+                            (substitute* "vex/vex.asd"
+                              (("maxpc-apache")
+                               "maxpc"))))))
+      (inputs (cons (list "sbcl-serapeum" sbcl-serapeum)
+                    (cons (list "sbcl-cl-unicode" sbcl-cl-unicode)
+                          (package-inputs sbcl-april)))))))
 
-(concatenate-manifests
- (list
-  (specifications->manifest '("sbcl"))
-  (packages->manifest (list sbcl-april-master))))
+(concatenate-manifests (list (specifications->manifest '("sbcl"))
+                             (packages->manifest (list sbcl-april-master))))
