4ccdf8f75a19cdfc1e21febb372d855ba65b5bd8
jaccarmac <jaccarmac@gmail.com>
2023-12-02 23:21:39 +0000

    Add TERM=vt100 discovery.
    FossilOrigin-Name: 28d7d809a691264635250686a681ba434cdb6a27c1e6e91bd3761a201199e3af

--- 2023/run-day.sh
+++ 2023/run-day.sh
@@ -2,3 +2,10 @@ DAY=$1
 DATA=$2
 
 lfe -eval "(c \"src/2023\")" -eval "(|2023:main| '(\"$DAY\" \"$DATA\"))"
+
+# TERM=vt100 guix shell -CNm manifest.scm -E TERM -- lfe
+#
+# Passing through TERM is the way to get the REPL to work inside a Guix
+# container. It even works for Rebar3's REPL, though Rebar3 will still not lfe
+# run-escript (some dependency on bash-minimal that's caught in the Guix
+# build.) <- TODO
