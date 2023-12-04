88b198d99f2dc4b9d0599884560058a4bd5791bb
jaccarmac <jaccarmac@gmail.com>
2023-12-04 01:55:48 +0000

    FHS emulation is necessary for Elm.
    FossilOrigin-Name: 7a0d000eb9ff39c42b5c3ed5bbc9ad817b16be8921459dcfe1d1d4bc2d27a1a2

--- run-day.sh
+++ run-day.sh
@@ -3,4 +3,4 @@ DAY=$2
 DATA=${3:-$DAY}
 
 cd $YEAR
-guix shell --container --network --manifest=manifest.scm -- ./run-day.sh $DAY $DATA
+guix shell --container --network --emulate-fhs --manifest=manifest.scm -- ./run-day.sh $DAY $DATA
