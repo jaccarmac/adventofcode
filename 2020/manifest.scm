55faccc90ab17fc097b838e3cb4d89e7a8eccc09
jaccarmac <jaccarmac@gmail.com>
2023-12-08 16:40:47 +0000

    Add Makefile for Kotlin.
    FossilOrigin-Name: 1afa61335c6a4186731d19174caa668aa0c090a04d7242061abbff517ae64edb

--- 2020/manifest.scm
+++ 2020/manifest.scm
@@ -1,3 +1,3 @@
 ;; kotlinc must be on the path. Guix doesn't take care of this... yet.
 (specifications->manifest
- '("openjdk" "coreutils-minimal" "bash"))
+ '("make" "curl" "nss-certs" "unzip" "openjdk" "coreutils-minimal" "bash"))
--- 2020/run-day.sh
+++ 2020/run-day.sh
@@ -1,6 +1,8 @@
 DAY=$1
 PATH=$(pwd)/kotlinc/bin:$PATH
 
+make kotlinc && touch kotlinc
+
 cd $DAY
 kotlinc $DAY.kt -include-runtime -d $DAY.jar
 java -jar $DAY.jar
